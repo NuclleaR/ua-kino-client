5a08853f84d6604185b4c324650e7e8d3af3b289
NuclleaR <nuclear.890@gmail.com>
2022-10-02 10:05:37 +0100

    Rename bundle Add icon

---
+++ android/app/src/main/kotlin/app/club/uakino/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.club.uakino
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/uakino/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.uakino
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
